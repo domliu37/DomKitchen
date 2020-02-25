55977b2289d576ef69fa862cb45b2086be0eecca
Dominic Liu <domliu37@hotmail.com>
2020-02-25 16:18:25 +1300

    Update Recipes language

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -9,15 +9,15 @@ export class RecipesService {
   private recipes: Recipe[] = [
     {
       id: 'r1',
-      title: '宫保鸡丁',
+      title: 'Kongbao chicken',
       imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT2dVb1PwR5KLYZrC8o0X62ekmoBxlVsz8vaFyyDAanKpux5xJn',
-      ingredients: ['鸡肉', '花生', '青红椒', '食用油']
+      ingredients: ['Chicken', 'Nuts', 'Vegs', 'Oil']
     },
     {
       id: 'r2',
-      title: '鱼香肉丝',
+      title: 'stired fried pork',
       imageUrl: 'https://i.pinimg.com/originals/68/cd/b3/68cdb3c1c32815762eb3a6ddaffe626e.jpg',
-      ingredients: ['猪肉', '胡萝卜', '木耳', '食用油']
+      ingredients: ['Pork Meat', 'Carrots', 'Mushrooms', 'Oil']
     }
   ];
 
